c65619da53199c2e1f17d12c7df614f948d90522
ASM Mostafa Rafid <rafid365.info@gmail.com>
2021-01-25 00:01:56 +0600

    finish

---
+++ assignment.js
@@ -0,0 +1,38 @@
+
+            //   kilometer to meter
+
+function kilometerToMeter(kilometer){
+    return meter = kilometer * 1000;    
+}
+var result =kilometerToMeter(20);
+console.log(result);
+
+                //bugestCalculator 
+function budgetCalculator(clock, phone, laptop) {
+  if(clock<0){
+    return 'Buying a clock'
+  }
+  if(phone<0){
+    return 'Buing a phone'
+  }
+  if(laptop<0){
+    return 'Buying a laptop'
+  }
+  const oneClock = 50 //clock
+  const onePhone = 100 //phone 
+  const oneLaptop = 500 //laptop
+  var totalClock = oneClock * clock;
+  var totalPhone = onePhone * phone;
+  var totalLaptop = oneLaptop * laptop;
+  var totalshopping = totalClock + totalPhone + totalLaptop;
+  return totalshopping;
+}
+var shopping = budgetCalculator(2,2,2);
+console.log (shopping);
+
+
+
+
+
+
+
